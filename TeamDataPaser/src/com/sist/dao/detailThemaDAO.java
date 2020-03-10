72880a99f0f2408d0baaac06ddcc4a76866c0636
sist <sist@DESKTOP-RVDJDNC>
2020-03-10 18:48:14 +0900

    2020 03 10

---
+++ TeamDataPaser/src/com/sist/dao/detailThemaDAO.java
@@ -0,0 +1,69 @@
+package com.sist.dao;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.*;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.sist.vo.DetailThemaVO;
+
+
+
+
+//connection pool
+
+public class detailThemaDAO {
+
+	private Connection conn;
+	private PreparedStatement ps;
+	private static detailThemaDAO dao; //싱클톤 만들기
+	
+	//연결
+	public void getConnection()
+	{
+		try
+		{
+			Context init=new InitialContext(); //탐색기 열기
+			Context c=(Context)init.lookup("java://comp//env");
+			DataSource ds=(DataSource)c.lookup("jdbc/oracle");
+			conn=ds.getConnection();
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();	
+		}
+		
+	}
+	
+	//반환
+	public void disConnection()
+	{
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+			
+		}catch(Exception ex) {}
+		
+	}
+	
+	public static detailThemaDAO newInstance()
+	{
+		if(dao==null)
+			dao=new detailThemaDAO();
+		return dao;
+		
+	}
+	
+	
+		
+	
+	
+	
+	
+}
